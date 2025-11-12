3da29727c7417562d7e7be04e352e2fa1ebf081e
beronikus <gradiefundu@gmail.com>
2025-11-12 21:47:38 +0100

    jump fixed, animation added
    fixed jump feel, added running and idle animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,47 +7,75 @@ public class Player : MonoBehaviour
 
     [SerializeField] private float playerMovementSpeed = 1f;
     [SerializeField] private float jumpHeigh = 1f;
+    [SerializeField] private float rayOffset = 0.05f;
+    [SerializeField] private float raycastLenght = 0.1f;
+    [SerializeField] private float coyoteTime = 0.3f;
+    [SerializeField] private float coyoteTimeDuration;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+  
+
     private Vector2 moveInput;
     private Rigidbody2D rb2D;
-    private bool isJumping = false;
+    private BoxCollider2D boxCollider2D;
+  
     
 
     private void Awake()
     {
         Instance = this;
         rb2D = GetComponent<Rigidbody2D>();
-        
+        boxCollider2D = GetComponent<BoxCollider2D>();
     }
+
+  
 
     private void FixedUpdate()
     {
         moveInput = PlayerInputAction.Instance.MoveActionPressed();
         rb2D.linearVelocity = new Vector2(moveInput.x * playerMovementSpeed, rb2D.linearVelocity.y);
+       
+        animator.SetBool("isRunning", Mathf.Abs(moveInput.x) > 0.01f);
 
-        if (PlayerInputAction.Instance.JumpActionPresssed() && isJumping == false)
+        if (moveInput.x > 0.01f)
         {
-            rb2D.linearVelocityY = jumpHeigh;
+            spriteRenderer.flipX = false;
+        }
+        else if (moveInput.x < -0.01f)
+        {
+            spriteRenderer.flipX = true;
         }
         
+        if (GetIsGrounded())
+        {
+            coyoteTimeDuration = coyoteTime;
+        }
+        else
+        {
+            coyoteTimeDuration -= Time.fixedDeltaTime;
+        }
+
+        if (PlayerInputAction.Instance.JumpActionPresssed() && coyoteTimeDuration > 0f)
+        {
+            rb2D.linearVelocityY = jumpHeigh;
+            coyoteTimeDuration = 0;
+
+        }
+
+       
     }
 
+    // Check ob Character auf den Boden ist.
+    private bool GetIsGrounded()
+    {
+        Vector2 rayOrigin = new Vector2(transform.position.x, boxCollider2D.bounds.min.y + rayOffset);
+        RaycastHit2D raycastDownHit2D = Physics2D.Raycast(rayOrigin, Vector2.down, raycastLenght, groundMask);
+        Vector2 raycastDirection = Vector2.down * raycastLenght;
+        
+        Debug.DrawRay(rayOrigin, raycastDirection, Color.blue );
+
+        return raycastDownHit2D.collider != null;
+    }
     
-
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.tag == "Ground")
-        {
-            isJumping = false;
-            Debug.Log("Touching the Ground");
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D other)
-    {
-        if (other.gameObject.tag == "Ground")
-        {
-            isJumping = true;
-            Debug.Log("Jumping");
-        }
-    }
 }
